4d48473014b81b316ad1352396f8f256dda6ccc7
David Scott <dave@recoil.org>
2016-04-03 10:39:34 +0100

    Allow the `docker-proxy` to be overridden
    This patch adds support for a new command-line argument
    `--userland-proxy-bin=""` alongside the existing `--userland-proxy`
    argument.
    
    A replacement proxy binary is a convenient place to perform some
    side-effect such as opening a tunnel to expose the port in some
    other place over some other protocol. For example where docker is
    running in a VM on a desktop, a replacement docker-proxy can use some
    out-of-band mechanism to bind a port on the host, and send/receive
    the data over some host<->VM channel such as AF_VSOCK.
    
    Other (less reliable) ways I've tried to solve the same problem include:
    
    - watch docker API events and create/destroy tunnels on demand.
      Unfortunately errors are handled asynchronously and cannot cause
      the container start to fail.
    
    - proxy the docker API itself and create/destroy tunnels during the
      API calls. This has the downside that it won't create tunnels when
      containers are automatically started by the daemon, and the
      docker API is larger (and faster moving) than the `docker-proxy` "API".
    
    The new --userland-proxy-bin argument is only inspected if
    --userland-proxy=true.
    
    If --userland-proxy-bin="" (the default) then the existing built-in
    docker-proxy will be used.
    
    Signed-off-by: David Scott <dave.scott@docker.com>

--- daemon/config_unix.go
+++ daemon/config_unix.go
@@ -42,6 +42,7 @@ type bridgeConfig struct {
 	EnableIPForward             bool   `json:"ip-forward,omitempty"`
 	EnableIPMasq                bool   `json:"ip-mask,omitempty"`
 	EnableUserlandProxy         bool   `json:"userland-proxy,omitempty"`
+	UserlandProxyBin            string `json:"userland-proxy-bin,omitempty"`
 	DefaultIP                   net.IP `json:"ip,omitempty"`
 	Iface                       string `json:"bridge,omitempty"`
 	IP                          string `json:"bip,omitempty"`
@@ -78,6 +79,7 @@ func (config *Config) InstallFlags(cmd *flag.FlagSet, usageFn func(string) strin
 	cmd.BoolVar(&config.bridgeConfig.InterContainerCommunication, []string{"#icc", "-icc"}, true, usageFn("Enable inter-container communication"))
 	cmd.Var(opts.NewIPOpt(&config.bridgeConfig.DefaultIP, "0.0.0.0"), []string{"#ip", "-ip"}, usageFn("Default IP when binding container ports"))
 	cmd.BoolVar(&config.bridgeConfig.EnableUserlandProxy, []string{"-userland-proxy"}, true, usageFn("Use userland proxy for loopback traffic"))
+	cmd.StringVar(&config.bridgeConfig.UserlandProxyBin, []string{"-userland-proxy-bin"}, "", usageFn("Use specific userland proxy binary if in userland proxy mode"))
 	cmd.BoolVar(&config.EnableCors, []string{"#api-enable-cors", "#-api-enable-cors"}, false, usageFn("Enable CORS headers in the remote API, this is deprecated by --api-cors-header"))
 	cmd.StringVar(&config.CorsHeaders, []string{"-api-cors-header"}, "", usageFn("Set CORS headers in the remote API"))
 	cmd.StringVar(&config.CgroupParent, []string{"-cgroup-parent"}, "", usageFn("Set parent cgroup for all containers"))
